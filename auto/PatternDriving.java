40967bc1ce91adcb2b72ba67680ba69a9b75bb6a
Dsgonzalez5 <149922070+Dsgonzalez5@users.noreply.github.com>
2025-09-29 19:15:07 -0400

    Add files via upload

--- auto/PatternDriving.java
+++ auto/PatternDriving.java
@@ -2,10 +2,32 @@ package edu.elon.robotics.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Pattern Driving")
-public class PatternDriving extends AutoCommon{
+@Autonomous(name = "Pattern Driving", group = "ShadowWizardMoneyGang")
+public class PatternDriving extends AutoCommon {
 
-    private final double[]  DRIVE_SEQ = {150, -150,   100,    -106.42,63.6};
-    private final double[] STRAFE_SEQ = {-90, 70, -250, -90};
+    private final double[] DRIVE_SEQ = {150, -50,-90, 100,70, -106.42,-250, 63.6,-90};
+    private final double SLOW_SPEED = 0.3;
 
+    //private final double[] STRAFE_SEQ = {-90, 70, -250, -90};
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+        waitForStart();
+
+        for (int i = 0; i < DRIVE_SEQ.length; i++) {
+            if (i == 0){
+                driveDistance(DRIVE_SEQ[i], 0, SLOW_SPEED);
+                if (!opModeIsActive()) return;
+                sleep(250);
+            } else if (i % 2 != 0){
+                driveDistance(DRIVE_SEQ[i], 0, SLOW_SPEED);
+                if (!opModeIsActive()) return;
+                sleep(250);
+            } else{
+                turnAngle(DRIVE_SEQ[i],SLOW_SPEED);
+            }
+
+        }
+    }
 }
