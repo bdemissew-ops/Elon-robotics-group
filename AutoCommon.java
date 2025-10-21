fb822c85ba31dd6248fa6359648893ccc7985bdf
bdemissew-ops <bdemissew@elon.edu>
2025-10-21 12:51:04 -0400

    Update AutoCommon.java

--- AutoCommon.java
+++ AutoCommon.java
@@ -2,6 +2,8 @@ package edu.elon.robotics.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 import edu.elon.robotics.base.RobotHardware;
 
 /*
@@ -113,7 +115,7 @@ public class AutoCommon extends LinearOpMode {
         robot.imu.resetYaw();
         robot.startMove(power, 0.0, 0.0);
 
-        double kP = 0.020;
+        double kP = 0.0185;
 
         while (opModeIsActive() && Math.abs(robot.motorLeft.getCurrentPosition()) < ticksLeftForward) {
             double error = robot.getHeading();
@@ -123,4 +125,121 @@ public class AutoCommon extends LinearOpMode {
         robot.startMove(power, 0.0, 0.0);
 
     }
+
+    protected void driveUntilTouch(double power){
+        robot.startMove(power, 0.0, 0.0);
+
+        boolean touchSensor = robot.touchSensor.isPressed();
+        System.out.println("touchSensor = " + touchSensor);
+        while (opModeIsActive() && !touchSensor){
+            touchSensor = robot.touchSensor.isPressed();
+        }
+        robot.startMove(0.0, 0.0, 0.0);
+    }
+
+    // rework later TODO FIXME
+    protected void driveCountTouch(double power){
+        driveToCalibrateLightSensor(power);
+        sleep(1000);
+
+        int count = 0;
+
+        int check = 0;
+        // move robot forward
+        robot.startMove(power, 0,0);
+
+        double avg = (robot.maxBrightness + robot.minBrightness)/2.0;
+        double buffer = avg * 0.3;
+
+
+        int lineWidthTicks = 0;
+
+        boolean touchSensor = robot.touchSensor.isPressed();
+        while (opModeIsActive() && !touchSensor){
+            touchSensor = robot.touchSensor.isPressed();
+
+
+
+            // collect data
+            telemetry.addData("Lines seen", count + " lines seen so far");
+            telemetry.update();
+            System.out.println("count = " + count);
+            System.out.println("robot.maxBrightness = " + robot.maxBrightness);
+
+
+            //when not grey update the count but also update the prevAlpha
+            if (robot.colorSensor.alpha() >= avg +buffer && check == 0) {
+                count++;
+                check = 1;
+                lineWidthTicks = robot.motorLeft.getCurrentPosition();
+
+                System.out.println("robot.colorSensor.alpha() = " + robot.colorSensor.alpha());
+            } else if (robot.colorSensor.alpha() < avg - buffer) { // small buffer
+                check = 0;
+
+                if (lineWidthTicks > 0){
+                    lineWidthTicks = robot.motorLeft.getCurrentPosition() - lineWidthTicks;
+                    double lineWidth = robot.convertTicksToDist(lineWidthTicks) + Math.cos(30) + .25;//test with cos(30)
+                    System.out.println("LINEWIDTH:  Line #" + count + " = " + lineWidth  + " cm");
+                }
+                lineWidthTicks = 0;
+            }
+
+
+
+
+        }
+        robot.startMove(0,0,0);
+
+        int checker = count +1;
+
+        robot.startMove(-power, 0,0);
+        while(opModeIsActive() && checker != 0){
+
+            if (robot.colorSensor.alpha() >= avg +buffer && check == 0) {
+                checker--;
+                check = 1;
+                System.out.println("robot.colorSensor.alpha() = " + robot.colorSensor.alpha());
+            } else if (robot.colorSensor.alpha() < avg - buffer) { // small buffer
+                check = 0;
+                System.out.println("robot.colorSensor.alpha() = " + robot.colorSensor.alpha());
+            }
+
+        }
+        robot.startMove(0,0,0);
+
+
+    }
+
+    protected void driveToCalibrateLightSensor(double power){
+        int maxVal = Integer.MIN_VALUE;
+
+        int minVal = Integer.MAX_VALUE;
+
+        int ticks = robot.convertDistToTicks(20);
+
+        robot.startMove(power, 0.0,0.0);
+
+        while (opModeIsActive() && robot.motorLeft.getCurrentPosition() < ticks){
+            System.out.println("robot.colorSensor.alpha() = " + robot.colorSensor.alpha());
+            if (robot.colorSensor.alpha() > maxVal){
+                maxVal = robot.colorSensor.alpha();
+            }
+
+            if (robot.colorSensor.alpha() < minVal){
+                minVal = robot.colorSensor.alpha();
+            }
+
+
+        }
+        System.out.println("maxVal = " + maxVal);
+        System.out.println("minVal = " + minVal);
+        robot.minBrightness = minVal;
+        robot.maxBrightness = maxVal;
+
+        robot.startMove(0.0, 0.0, 0.0);
+
+    }
+
+
 }
